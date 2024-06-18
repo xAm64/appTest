f5347b8fbf7af3679fa4988817db7bf88492c369
xAm64 <48089283+xAm64@users.noreply.github.com>
2024-06-18 14:28:39 +0200

    suppr ancienne extensions

--- wp-content/plugins/eventprime-event-calendar-management/includes/core/admin/blocks/build/ep-booking-details-block/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c7a83e89e1262a27eb05');
--- wp-content/plugins/eventprime-event-calendar-management/includes/core/admin/blocks/build/ep-register-block/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '21483ce1e7875e751e91');
